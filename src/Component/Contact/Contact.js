f51d5cff6143c4bb41ca501edb2ee58ff93caee9
neelkamal <kneel062@gmail.com>
2024-07-16 08:19:45 +0530

    Adding changes in Contact, Extra, Value component

--- src/Component/Contact/Contact.js
+++ src/Component/Contact/Contact.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import "./Contact.css"
+import { MdCall } from 'react-icons/md';
+import { HiChatBubbleBottomCenter } from 'react-icons/hi2';
+import { BsFillChatDotsFill } from 'react-icons/bs';
 export default function Contact() {
     return (
         <section className="c-wrapper">
@@ -11,15 +14,74 @@ export default function Contact() {
                     <span className='secondaryText'>
                         Lorem ipsum dolor, sit amet
                         consectetur adipisicing elit.
-                        Nulla quaerat animi volun 
+                        Nulla quaerat animi volun
                         consectetur adipisicing elit.
                         consectetur adipisicing elit.
-                        Nulla quaerat animi volun 
+                        Nulla quaerat animi volun
                         consectetur adipisicing elit.
-                        consectetur adipisicing elit.</span>
+                        consectetur adipisicing elit.
+                    </span>
+                    <div className="flexColStart contactModes">
+                        {/* first-row */}
+                        <div className="flexStart row">
+                            <div className="flexColCenter mode">
+                                <div className="flexStart">
+                                    <div className="flexCenter icon">
+                                        <MdCall size={25} />
+                                    </div>
+                                    <div className="flexColStart detail ">
+                                        <span className='primaryText'>Call</span>
+                                        <span className='secondaryText'>758 584 398</span>
+                                    </div>
+                                </div>
+                                <div className="flexCenter button ">Call Now</div>
+                            </div>
+                        
+                            <div className="flexColCenter mode">
+                                <div className="flexStart">
+                                    <div className="flexCenter icon">
+                                        <BsFillChatDotsFill size={25} />
+                                    </div>
+                                    <div className="flexColStart detail ">
+                                        <span className='primaryText'>Chat</span>
+                                        <span className='secondaryText'>758 584 398</span>
+                                    </div>
+                                </div>
+                                <div className="flexCenter button ">Chat Now</div>
+                            </div>
+                        </div>
+                            {/* Second-row */}
+                            <div className="flexStart row">
+                                <div className="flexColCenter mode">
+                                    <div className="flexStart">
+                                        <div className="flexCenter icon">
+                                            <HiChatBubbleBottomCenter size={25} />
+                                        </div>
+                                        <div className="flexColStart detail ">
+                                            <span className='primaryText'>Video Call</span>
+                                            <span className='secondaryText'>758 584 398</span>
+                                        </div>
+                                    </div>
+                                    <div className="flexCenter button ">Chat Now</div>
+                                </div>
+
+                                <div className="flexColCenter mode">
+                                    <div className="flexStart">
+                                        <div className="flexCenter icon">
+                                            <HiChatBubbleBottomCenter size={25} />
+                                        </div>
+                                        <div className="flexColStart detail ">
+                                            <span className='primaryText'>Message</span>
+                                            <span className='secondaryText'>758 584 398</span>
+                                        </div>
+                                    </div>
+                                    <div className="flexCenter button ">Message Now</div>
+                                </div>
+                            </div>
+                    </div>
                 </div>
                 {/* right-side */}
-                <div className="c-right">
+                <div className="flexEnd c-right">
                     <div className="image-container">
                         <img src="./assests/main4.jpg" alt="" />
                     </div>
